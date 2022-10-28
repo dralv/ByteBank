274d0e32b66c3c260b00abedf889319d72c718ed
Alvaro <dralvphd2@gmail.com>
2022-10-27 21:15:08 -0300

    Adicionando métodos

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -9,3 +9,25 @@ contaAlvaro.saldo = 100;
 
 Console.WriteLine("Saldo do Álvaro " + contaAlvaro.saldo);
 
+contaAlvaro.Depositar(50);
+
+Console.WriteLine("Saldo do Álvaro pós-deposito " + contaAlvaro.saldo);
+
+if(contaAlvaro.Sacar(50)==true)
+    Console.WriteLine("Saldo do Álvaro pós-saque " + contaAlvaro.saldo);
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque");
+}
+
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.numeroAgencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+
+Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+contaAlvaro.Transferir(50, contaDaMaria);
+Console.WriteLine("Saldo do Alvaro = " + contaAlvaro.saldo);
+Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
