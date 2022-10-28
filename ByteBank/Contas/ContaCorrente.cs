ce84a32d7ef81681cadbb18256c85246aa6766a7
Alvaro <dralvphd2@gmail.com>
2022-10-28 08:47:39 -0300

    Criando a classe de cliente, organizando pastas e namespaces

--- ByteBank/Contas/ContaCorrente.cs
+++ ByteBank/Contas/ContaCorrente.cs
@@ -3,43 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Titular;
 
-namespace ByteBank
+namespace ByteBank.Contas
 {
     public class ContaCorrente
     {
         public int numeroAgencia;
         public string conta;
-        public string titular;
         public double saldo = 100;
+        public Cliente titular;
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            saldo += valor;
         }
+        
+        
         public bool Sacar(double valor)
-        {   
-            if(valor <= this.saldo)
+        {
+            if (valor <= saldo)
             {
-                this.saldo -= valor;
+                saldo -= valor;
                 return true;
             }
             else
             {
                 return false;
             }
-            
+
         }
 
         public bool Transferir(double valor, ContaCorrente destino)
         {
-            if(this.saldo < valor)
+            if (saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.Sacar(valor);
+                Sacar(valor);
                 destino.Depositar(valor);
                 return true;
             }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,40 +1,65 @@
-﻿using ByteBank;
+﻿using ByteBank.Contas;
+using ByteBank.Titular;
 
-ContaCorrente contaAlvaro = new ContaCorrente();
+//ContaCorrente contaAlvaro = new ContaCorrente();
 
-contaAlvaro.titular = "Álvaro Teixeira";
-contaAlvaro.numeroAgencia = 15;
-contaAlvaro.conta = "1010-x";
-contaAlvaro.saldo = 100;
+//contaAlvaro.titular = "Álvaro Teixeira";
+//contaAlvaro.numeroAgencia = 15;
+//contaAlvaro.conta = "1010-x";
+//contaAlvaro.saldo = 100;
 
-Console.WriteLine("Saldo do Álvaro " + contaAlvaro.saldo);
+//Console.WriteLine("Saldo do Álvaro " + contaAlvaro.saldo);
 
-contaAlvaro.Depositar(50);
+//contaAlvaro.Depositar(50);
 
-Console.WriteLine("Saldo do Álvaro pós-deposito " + contaAlvaro.saldo);
+//Console.WriteLine("Saldo do Álvaro pós-deposito " + contaAlvaro.saldo);
 
-if(contaAlvaro.Sacar(50)==true)
-    Console.WriteLine("Saldo do Álvaro pós-saque " + contaAlvaro.saldo);
-else
-{
-    Console.WriteLine("Saldo insuficiente para saque");
-}
+//if(contaAlvaro.Sacar(50)==true)
+//    Console.WriteLine("Saldo do Álvaro pós-saque " + contaAlvaro.saldo);
+//else
+//{
+//    Console.WriteLine("Saldo insuficiente para saque");
+//}
 
-ContaCorrente contaDaMaria = new ContaCorrente();
-contaDaMaria.titular = "Maria Souza";
-contaDaMaria.numeroAgencia = 17;
-contaDaMaria.conta = "1010-5";
-contaDaMaria.saldo = 350;
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numeroAgencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
 
-Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+//Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
 
-contaAlvaro.Transferir(50, contaDaMaria);
-Console.WriteLine("Saldo do Alvaro = " + contaAlvaro.saldo);
-Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+//contaAlvaro.Transferir(50, contaDaMaria);
+//Console.WriteLine("Saldo do Alvaro = " + contaAlvaro.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
 
-ContaCorrente contaPedro = new ContaCorrente();
-contaPedro.titular = "Pedro Silva";
-Console.WriteLine(contaPedro.titular);
-Console.WriteLine(contaPedro.saldo);
-Console.WriteLine(contaPedro.numeroAgencia);
-Console.WriteLine(contaPedro.conta);
+//ContaCorrente contaPedro = new ContaCorrente();
+//contaPedro.titular = "Pedro Silva";
+//Console.WriteLine(contaPedro.titular);
+//Console.WriteLine(contaPedro.saldo);
+//Console.WriteLine(contaPedro.numeroAgencia);
+//Console.WriteLine(contaPedro.conta);
+
+Cliente cliente = new Cliente();
+cliente.nome = "Álvaro Teixeira";
+cliente.cpf = "85696574178";
+cliente.profissao = "Desenvolvedor";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numeroAgencia = 15;
+conta.saldo = 100;
+
+Console.WriteLine("Titular = " + conta.titular);
+
+ContaCorrente conta2 = new ContaCorrente();
+
+conta2.titular = new Cliente();
+conta2.titular.nome = "José Souza";
+conta2.titular.profissao = "Tester";
+conta2.titular.cpf = "78945613274";
+conta2.conta = "9999-x";
+conta2.numeroAgencia = 18;
+conta2.saldo = 250;
+
